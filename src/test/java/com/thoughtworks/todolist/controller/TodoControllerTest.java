ab966f63e69c4155f804f0ed3de285edd6749cbc
Ayush Saraf <ayush.saraf47@gmail.com>
2020-02-12 17:36:56 +0530

    Added: test for scenario where there are no todos to return for get-todos request

--- src/test/java/com/thoughtworks/todolist/controller/TodoControllerTest.java
+++ src/test/java/com/thoughtworks/todolist/controller/TodoControllerTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.todolist.controller;
 
+import com.thoughtworks.todolist.exception.NoteNotFoundException;
 import com.thoughtworks.todolist.model.ToDoNote;
 import com.thoughtworks.todolist.service.ToDoListService;
 import org.junit.Assert;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -39,10 +41,21 @@ class TodoControllerTest {
     }
 
     @Test
-    void getAllToDoNotes() {
+    void givenARequestToGetAllToDoNotes_WhenAvailable_ShouldReturnListOfToDos() {
         when(toDoListService.getToDoList()).thenReturn(toDoNoteList);
         ResponseEntity<List<ToDoNote>> allToDoNotes = controller.getAllToDoNotes();
         Assert.assertEquals(200,allToDoNotes.getStatusCode().value());
+    }
+
+
+    @Test
+    void givenARequestToGetAllToDoNotes_WhenNotAvailable_ShouldReturnListOfToDos() {
+        try {
+            when(toDoListService.getToDoList()).thenThrow(new NoteNotFoundException("No Notes!!", HttpStatus.NOT_FOUND));
+            ResponseEntity<List<ToDoNote>> allToDoNotes = controller.getAllToDoNotes();
+        } catch (NoteNotFoundException e) {
+            Assert.assertEquals(404,e.statusCode.value());
+        }
     }
 
 
